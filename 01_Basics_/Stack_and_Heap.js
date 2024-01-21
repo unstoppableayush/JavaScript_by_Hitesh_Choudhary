3f7a972818babfd286d580faf4bd8fa943c616a3
unstoppableayush <ayushkum884@gmail.com>
2024-01-21 17:53:21 +0530

    learned about stack and heap where variables are allocated

---
+++ 01_Basics_/Stack_and_Heap.js
@@ -0,0 +1,10 @@
+/*
+
+//datatypes are based on memory allocation
+Primitive datatypes->  call by value (string,number,boolean,null,undefined,BigInt,symbol) ->Stack memory
+
+Reference/Non primitive data types -> call by reference ( array,object, function) -> heap memory
+
+JavaScript is a dynamically typed language. This means that you don't need to specify the data type of a variable when you declare it.
+
+*/
