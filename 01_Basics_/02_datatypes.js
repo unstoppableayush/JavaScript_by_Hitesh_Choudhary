b9af9adbd59615b09b3848d85c3c2b1d4c333526
unstoppableayush <ayushkum884@gmail.com>
2024-01-21 16:48:23 +0530

    conersions of datatype

---
+++ 01_Basics_/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //We are using nodejs , not browser
+
+//for the readability of the code we avoid using semicolon
+
+console.log(3 + 3)
+console.log("Ayush ")
+
+
+let name = "Ayush"
+let age = 18
+let isLoggedIn = false
+
+/*
+
+Primtive datatype
+-----------------------
+
+number => 2 to power 53
+bigInt => 
+String => ""
+boolean => true/false
+null => standalone value
+undefined => variable is declared but not defined the value
+symbol =>  unique
+
+Non primitive
+
+Object 
+
+*/
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);      //object
---
+++ 01_Basics_/03_convesionOperation.js
@@ -0,0 +1,41 @@
+// let score = "33abc"
+// let score =null
+let score = "ayush"
+
+// console.log(typeof(score))
+// console.log(typeof score)
+
+//conversion to string
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//"33abc" => NaN -> Not a Number
+// true => 1 ; false => 0
+
+// let isLoggedIn = 1
+let isLoggedIn = ""
+// let isLoggedIn = "Ayush"
+
+
+//conversion to boolean
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+
+// 1+> true ; 0 => false
+// "" => false => only for empty string not "  "
+// "Ayush" => true
+
+//conversion to string
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
