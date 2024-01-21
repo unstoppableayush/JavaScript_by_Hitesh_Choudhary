963c7c6f2a5adf0e637b2f67e1ff32ef340b075d
unstoppableayush <ayushkum884@gmail.com>
2024-01-21 17:16:40 +0530

    operations on datatype done

--- 01_Basics_/03_convesionOperation.js
+++ 01_Basics_/03_convesionOperation.js
@@ -33,9 +33,36 @@ let booleanisLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 
+// **********************Operations*****************
 
+let value = 3
+
+let negValue = -value
+
+// console.log(negValue);
+
+let str1 = "Hello "
+let str2 = "Ayush"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" +2)  //12
+// console.log(1 +"2")  //12
+// console.log("1" +2 + 2) //122
+// console.log(1 +2 + "2") //32
+
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter
+
+console.log(gameCounter);
 
