c29ea221c0418f7cd47d8e8d12ceb1ae6dc065d2
unstoppableayush <ayushkum884@gmail.com>
2024-01-28 19:45:00 +0530

    keraned about iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,15 @@
+// Immediately Invocked Function Expression (IIFE)
+
+(function chai(){
+    console.log(`DB CONNECTED`)
+})();
+
+//named iife
+((name)=>{
+    console.log(`DB CONNECTED TWO ${name}`);
+})("Ayush");
+
+
+
+
+
