1e5592047afc92d0330530992a1cdaedcb9b1bad
unstoppableayush <ayushkum884@gmail.com>
2024-01-28 17:55:39 +0530

    function with object and array learned

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -18,7 +18,7 @@ function addTwoNumbers(number1 , number2){
 
 const result = addTwoNumbers( 3 ,5)  //arguments
 
-console.log("Result:" , result)
+// console.log("Result:" , result)
 
 function loginUserMessage(username){
     // if(username ===undefined){
@@ -30,4 +30,39 @@ function loginUserMessage(username){
 }
 
 
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+// function with objects and array
+
+function calculateCartPrice(val1 , val2 ,...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200 , 400 , 500))
+
+const user = {
+    username: "hitesh",
+    price:199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// handleObject(user);
+
+handleObject({
+    username:"sam",
+    price:399
+})
+
+
+const myNewArray = [200 , 400 , 100 , 600]
+
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log([200 , 400 , 100 , 600]);
