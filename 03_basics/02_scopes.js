dbd2a9e61ee172623c462c7f69ddfb035b80650d
unstoppableayush <ayushkum884@gmail.com>
2024-01-28 19:17:43 +0530

    learned about scopes and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -7,10 +7,51 @@ const b = 20
 if(true){
     let a = 10
     const b = 20
-    console.log("Inner" , a)
+    // console.log("Inner" , a)
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+//nested scope
+
+function one(){
+    const username = "ayush"
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+    //console.log(website);
+    two()
+}
+// one()
+
+
+if(true){
+    const username ="hitesh"
+    if(username === "hitesh"){
+        const website = "youtube"
+        // console.log(username + website)
+    }
+    // console.log(website)
+}
+// console.log(username)
+
+
+// ++++++++++++++++ Interesting +++++++++++++++
+
+console.log(add(4))
+
+function add(num){
+    return num+1
+}
+
+
+
+//expression
+const addTwo = function add(num){
+    return num+1
+}
+console.log(addTwo(5))
