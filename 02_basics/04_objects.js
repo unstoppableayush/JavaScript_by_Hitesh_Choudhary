cd24f81f1ac4a1df374d966cf4a0c7ee3934526e
unstoppableayush <ayushkum884@gmail.com>
2024-01-28 17:17:25 +0530

    learned more about objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,62 @@
+//Singleton
+
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id ="12abc"
+tinderUser.name = "sammy"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser);
+
+const regularUser = {
+    email:"some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"ayush"
+            ,
+            lastname:"Kumar"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+//combining the objects
+
+const obj1 = {1:"a" , 2:"b"}
+const obj2 = {3:"a" , 4:"b"}
+
+// const obj3 = {obj1 , obj2}
+// const obj3 = Object.assign({},   obj1 , obj2)
+                        //target   sources
+
+
+//using spread operator
+
+const obj3 = {...obj1 , ...obj2}
+console.log(obj3);
+
+const users = [
+    {
+        id:1,
+        email:"a@gmail.com"
+    },
+    {
+        id:2,
+        email:"ay@gmail.com"
+    },
+    {
+        id:3,
+        email:"ayu@gmail.com"
+    }
+]
+
+users[1].email
+console.log(tinderUser);
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+
