c38e848f9c02c078a3c7de9cef5fa75c60b2e05c
unstoppableayush <ayushkum884@gmail.com>
2024-01-28 13:34:24 +0530

    array and array methods learned

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -12,3 +12,41 @@ const myHeros = ["Shaktiman" , "Naagraj"]
 
 const myArr2 = new Array(1 ,2 ,3 , 4)
 console.log(myArr2[1])
+
+//Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(3));
+
+// const newArr= myArr.join()
+
+// console.log(myArr);
+// console.log(typeof newArr); //string
+
+//slice , spice
+
+console.log("A" , myArr);
+
+const myn1 = myArr.slice( 1, 3)
+
+console.log(myn1);
+console.log("B" , myArr);
+
+const myn2 = myArr.slice( 1, 3)
+console.log(myn2);
+console.log("C" , myArr); 
+
+const myn3 = myArr.splice( 1, 3)
+console.log(myn3);
+console.log("D" , myArr); //manipulate original array
+
+
+
+
